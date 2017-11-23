bc1e99b820c051c4cf9a6ab54a55a0ed0a15611e
Cristian Peñarrieta <cristian.penarrieta@gmail.com>
2017-11-22 21:00:05 -0800

    formating output

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-var regex = /A(B|C+)+D/
-// var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-// const regex = /^([a-zA-Z0-9])(([\-.]|[_]+)?([a-zA-Z0-9]+))*(@){1}[a-z0-9]+[.]{1}(([a-z]{2,3})|([a-z]{2,3}[.]{1}[a-z]{2,3}))$/
+var tripwire = require('tripwire')
+var chalk = require('chalk')
+
+// var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ //evil
+// const regex = /^([a-zA-Z0-9])(([\-.]|[_]+)?([a-zA-Z0-9]+))*(@){1}[a-z0-9]+[.]{1}(([a-z]{2,3})|([a-z]{2,3}[.]{1}[a-z]{2,3}))$/ //evil
+// var regex = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/ // not evil
+const regex = /A(B|C+)+D/ //evil
+
+const iterations = 10
+const timeoutEvilRegex = 1500
+
+process.on('uncaughtException', function (e) {
+  console.log(`Took more than ${timeoutEvilRegex}ms. ${chalk.red('This is most likely an evil regular expression')}`)
+  process.exit(1)
+})
+
 const randexp = require('./wrong-string-regex').randexp
 
-const results = Array.from({ length: 10 }).map((e, k) => {
-  return randexp(regex)
+const incorrectStrings = Array.from({ length: iterations }).map((e, k) => randexp(regex))
+
+incorrectStrings.forEach(s => {
+  console.log(`evaluating: ${chalk.yellow(s)}`)
+  tripwire.resetTripwire(timeoutEvilRegex)
+  console.time(chalk.green(s))
+  regex.test(s)
+  console.timeEnd(chalk.green(s))
 })
-console.log(results)
+
+console.log('')
+console.log(chalk.green('-----  Your regex is fine -----'))
+
+tripwire.clearTripwire()
