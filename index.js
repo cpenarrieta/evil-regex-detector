55e49b53fce55c2d00430f79ed85cbfebad2fb93
Cristian Peñarrieta <cristian.penarrieta@gmail.com>
2017-12-09 15:54:42 -0800

    adding regex to txt files

--- index.js
+++ index.js
@@ -1,60 +1,7 @@
 var tripwire = require('tripwire')
 var chalk = require('chalk')
 
-// var regex = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
-
 const regex = /A(B|C+)+D/ 
-// const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-// const regex = /^([a-zA-Z0-9])(([\-.]|[_]+)?([a-zA-Z0-9]+))*(@){1}[a-z0-9]+[.]{1}(([a-z]{2,3})|([a-z]{2,3}[.]{1}[a-z]{2,3}))$/
-// const regex = /(\w+\d+)+C/
-// const regex = /^(([a-z])+.)+[A-Z]([a-z])+$/
-// const regex = /^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]).)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9-]*[A-Za-z0-9])$/
-// const regex = /^([^.\/]+\/?)*$/
-// const regex = /D[oD]?(\[[^\[\]]*\]|\s+)+MMMM?/
-// const regex = /^(`+)\s*([\s\S]*?[^`])\s*\1(?!`)/
-// const regex = /(([\w#:.~>+()\s-]+|[.?])+)\s(,|$)/
-
-// const regex = /^[\w-]+(?:\.[\w-]+)*@(?:[\w-]+\.)+[a-zA-Z]{2,7}$/
-
-//----
-// # Vulnerable
-// const regex = /(a|ab|b)*c|.*/ // detected
-
-// # No suffix
-// const regex = /.*|(a|ab|b)*c/ // not detected
-
-// # Empty suffix
-// const regex = /.*c|(a|ab|b)*c/ // detected
-
-// # No suffix
-// const regex = /.*b|(a|ab|b)*c/ // not detected
-
-// # Phi2 < Phi3
-// const regex = /(.*b|(a|ab|b)*c)d/ // detected
-
-// # Unroll Kleene star
-// const regex = /a.*|(b|a|ab)*c/ // detected
-
-// # X1 Choice
-// const regex = /c.*|(c|d)(a|b|ab)*e/  // detected
-
-// # Pumpable choice
-// const regex = /d.*|((c|d)(a|a))*b/ // detected
-
-// # Unroll c*
-// const regex = /a.*|(c*a(b|b))*d/ // detected
-
-// # Only b is pumpable
-// const regex = /(a|a|b|b)*(a.*|c)/ // not detected
-
-// # Unroll c*
-// const regex = /(a|b).*|c*(a|b|ab)*d/ // detected
-
-// # Unroll selective
-// const regex = /(ab|a*b|aab.*)*$/ // detected
-
-// # lexing error
-// const regex = /b(([\w-]+://?|www[.])[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/)))/ // invalid regex
 
 const iterations = 10
 const timeoutEvilRegex = 1500
